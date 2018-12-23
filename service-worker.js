16ad75fb28327f2de05f6be3e9e1f195e09468a6
Marco Lindgren <marcolindgren93@gmail.com>
2018-12-23 11:29:43 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/2018-interactive-web-apps-project-marcolindgren93/precache-manifest.7ae18c549698c861aae1898d6b2cee57.js"
+  "/2018-interactive-web-apps-project-marcolindgren93/precache-manifest.81bf1bd155418f0d2fe09de1424d2fd2.js"
 );
 
 workbox.clientsClaim();
